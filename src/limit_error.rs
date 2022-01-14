9da86a4d312179efbbe4793b6ddea1a8688f17b3
Markus Kolb <github@tower-net.de>
2022-01-14 22:40:21 +0100

    feat!: LimitError::GovernedRequest(u64, Quota)
    also some docs added/modified

--- src/limit_error.rs
+++ src/limit_error.rs
@@ -1,6 +1,7 @@
-//! Errors for governed requests which implement [Responder](rocket::response::Responder).
+//! Errors for governed requests which implement 
+//! [Responder](rocket::response::Responder).
 
-use super::header::Header;
+use super::{header::Header, Quota};
 use rocket::{
     response::{self, Responder},
     Request,
@@ -8,26 +9,36 @@ use rocket::{
 
 mod catcher;
 
-/// Errors for governed requests which implement [Responder](rocket::response::Responder).
+/// Errors for governed requests which implement 
+/// [Responder](rocket::response::Responder).
 #[derive(Clone, Debug)]
 pub enum LimitError {
     /// Any other undefined LimitError
     Error,
 
+    // TODO: Check ratelimit headers DRAFT for publication
     /// Governed request for the next provided seconds.
-    GovernedRequest(u64),
+    /// Provided `Quota` will be used for setting additional
+    /// HTTP headers defined by
+    /// https://datatracker.ietf.org/doc/html/draft-ietf-httpapi-ratelimit-headers
+    /// These headers could be used in service clients to use the service
+    /// in a more compliant way for its resources.
+    GovernedRequest(u64, Quota),
 
-    /// There is no remote client IP address known in the request. Might be a misconfigured server environment.
+    /// There is no remote client IP address known in the request. Might be 
+    /// a misconfigured server environment.
     MissingClientIpAddr,
 
     /// Route is not available which might be only the case in fairings
     MissingRoute,
 
-    /// There is a route without name and this can not be matched for rate limiting
+    /// There is a route without name and this can not be matched for 
+    /// rate limiting
     MissingRouteName,
 }
 
-/// Implements [Responder](rocket::response::Responder) to provide [Result](rocket::response::Result) possibilities.
+/// Implements [Responder](rocket::response::Responder) to provide 
+/// [Result](rocket::response::Result) possibilities.
 impl<'r, 'o: 'r> Responder<'r, 'o> for &LimitError {
     fn respond_to(self, request: &'r Request<'_>) -> response::Result<'o> {
         let mut handler = catcher::too_many_requests_handler(request);
@@ -36,9 +47,17 @@ impl<'r, 'o: 'r> Responder<'r, 'o> for &LimitError {
             LimitError::Error => {
                 handler.set_header(Header::XRateLimitError("rate limiter error"));
             }
-            LimitError::GovernedRequest(wait_time) => {
+            LimitError::GovernedRequest(wait_time, quota) => {
                 handler.set_header(Header::RetryAfter(*wait_time));
-                handler.set_header(Header::XRateLimitReset(*wait_time));
+                // TODO: x-ratelimit-limit can describe the time window of limit
+                //       https://datatracker.ietf.org/doc/html/draft-ietf-httpapi-ratelimit-headers#section-5.1
+                handler.set_header(Header::XRateLimitLimit(quota.burst_size().get() as u64));
+                // XRateLimitRemaining makes no sense here in LimitError
+                // because `state.remaining_burst_capacity()` should be 
+                // always 0 
+                handler.set_header(Header::XRateLimitReset(
+                    quota.burst_size_replenished_in().as_secs(),
+                ));
             }
             LimitError::MissingClientIpAddr => {
                 handler.set_header(Header::XRateLimitError(
